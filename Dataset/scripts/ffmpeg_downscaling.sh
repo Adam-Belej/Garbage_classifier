22bdd53e80ca743ec45c930adedfd1b6e249aa1f
Jan Černý <jc@ucw.cz>
2023-09-14 19:12:49 +0200

    add status report to ffmpeg_downscaling script

--- Dataset/scripts/ffmpeg_downscaling.sh
+++ Dataset/scripts/ffmpeg_downscaling.sh
@@ -7,6 +7,7 @@ height=512
 
 for input_file in "$input_folder"/*; do
     if [ -f "$input_file" ]; then
+        echo "Processing $input_file"
         filename=$(basename "$input_file")
         output_file="$output_folder/${filename%.*}.png"
         ffmpeg -i "$input_file" -vf "scale=$width:$height" "$output_file"

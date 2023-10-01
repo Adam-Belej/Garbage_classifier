200fdf36972f88b59a776e9dae24ed2fb3468421
Adam Belej <adam.belej@seznam.cz>
2023-10-01 17:35:18 +0200

    refactor katna script to remove global variables

--- Dataset/scripts/katna_script_squares.py
+++ Dataset/scripts/katna_script_squares.py
@@ -4,51 +4,45 @@ import os
 import sys.argv
 import typing
 
-# DIRECTORY = "/home/adam/Projects/Garbage_Classifier_dataset/Plast"
-# DIRECTORY_OUT = "/home/adam/Projects/Garbage_Classifier_dataset_squares"
-CROP_ASPECT_RATIO = "1:1"
-IMAGE_EXTENSIONS = [".jpg", ".jpeg", ".png", ".bmp", ".gif"]
-NUM_OF_CROPS = 1
-DOWN_SAMPLE_FACTOR = 32
 
-
-
-def get_all_images_from_dir(directory: str): -> [str]:
+def get_all_images_from_dir(directory: str,
+                            image_extensions=None) -> [str]:
+    if image_extensions is None:
+        image_extensions = [".jpg", ".jpeg", ".png", ".bmp", ".gif"]
     images = []
     for filename in os.listdir(directory):
-        if any(filename.endswith(ext) for ext in IMAGE_EXTENSIONS):
+        if any(filename.endswith(ext) for ext in image_extensions):
             images.append(os.path.join(directory, filename))
     return images
 
 
-def crop_image(image_path, directory, directory_out):
-    image_file_path = os.path.join(directory, filename)
+def crop_image(directory: str,
+               directory_out: str,
+               crop_aspect_ratio: str = "1:1",
+               num_of_crops: int = 1,
+               down_sample_factor: int = 32):
     img_module = Image()
     diskwriter = ImageCropDiskWriter(location=directory_out)
 
     crop_list = img_module.crop_image_with_aspect(
-        file_path=image_file_path,
-        crop_aspect_ratio=CROP_ASPECT_RATIO,
-        num_of_crops=NUM_OF_CROPS,
+        file_path=directory,
+        crop_aspect_ratio=crop_aspect_ratio,
+        num_of_crops=num_of_crops,
         writer=diskwriter,
-        down_sample_factor=DOWN_SAMPLE_FACTOR
+        down_sample_factor=down_sample_factor
     )
     return crop_list
 
 
-
 def main():
-    DIRECTORY = input("Directory in: ")
-    DIRECTORY_OUT = input("Directory out: ")
-    print("Directory: ", DIRECTORY)
+    directory = input("Directory in: ")
+    directory_out = input("Directory out: ")
+    print("Directory: ", directory)
 
     for filename in get_all_images_from_dir(directory):
         print("Cropping: ", filename)
-        crop_image(filename, DIRECTORY, DIRECTORY_OUT)
-
+        crop_image(filename, directory_out)
 
 
 if __name__ == "__main__":
     main()
-
-

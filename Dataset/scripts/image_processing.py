bc9b80896ca7b767618bf2baa0bbfef56244e0dc
Adam Belej <adam.belej@seznam.cz>
2023-10-02 15:07:06 +0200

    added first version of the full resizing + augmenting script

---
+++ Dataset/scripts/image_processing.py
@@ -0,0 +1,25 @@
+from katna_script_squares import get_all_images_from_dir, crop_image_with_aspect_ratio
+from ffmpeg_dataset_tools import downscale_image, rotate_image, horizontal_flip_image
+
+
+path_to_images = str(input("Folder with images: "))
+original_images = get_all_images_from_dir(path_to_images)
+
+
+cropped_images = []
+for image in original_images:
+    cropped_images.append(crop_image_with_aspect_ratio(image, f"{path_to_images}_cropped"))
+
+
+downscaled_images = []
+for cropped_image in cropped_images:
+    downscale_image(cropped_image, f"{cropped_image}_downscaled")
+    downscaled_images.append(f"{cropped_image}_downscaled")
+
+
+if str(input("Augment images? Y/N: ")).lower() == "y":
+    for downscaled_image in downscaled_images:
+        horizontal_flip_image(downscaled_image, f"{downscaled_image}_flipped")
+        for i in range(1, 5):
+            rotate_image(downscaled_image, f"{downscaled_image}_rotated_{90*i}")
+            rotate_image(f"{downscaled_image}_flipped", f"{downscaled_image}_flipped_rotated_{90*i}")

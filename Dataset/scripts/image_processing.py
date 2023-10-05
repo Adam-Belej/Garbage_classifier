bbf6c494c45c05feb365e0752df983d98a99fe30
Adam Belej <adam.belej@seznam.cz>
2023-10-05 18:36:09 +0200

    rewritten parts of the codes, wip

--- Dataset/scripts/image_processing.py
+++ Dataset/scripts/image_processing.py
@@ -1,3 +1,4 @@
+import os
 from katna_script_squares import get_all_images_from_dir, crop_image_with_aspect_ratio
 from ffmpeg_dataset_tools import downscale_image, rotate_image, horizontal_flip_image
 
@@ -6,20 +7,23 @@ path_to_images = str(input("Folder with images: "))
 original_images = get_all_images_from_dir(path_to_images)
 
 
-cropped_images = []
+cropped_path = f"{path_to_images}_cropped"
 for image in original_images:
-    cropped_images.append(crop_image_with_aspect_ratio(image, f"{path_to_images}_cropped"))
+    image = os.path.join(path_to_images, image)
+    crop_image_with_aspect_ratio(image, f"{path_to_images}_cropped")
 
 
-downscaled_images = []
+downscaled_path = f"{cropped_path}_downscaled"
+cropped_images = get_all_images_from_dir(cropped_path)
 for cropped_image in cropped_images:
-    downscale_image(cropped_image, f"{cropped_image}_downscaled")
-    downscaled_images.append(f"{cropped_image}_downscaled")
+    cropped_image = os.path.join(cropped_path, cropped_image)
+    downscaled_image = os.path.join(downscaled_path, cropped_image)
+    downscale_image(cropped_image, downscaled_image)
 
 
-if str(input("Augment images? Y/N: ")).lower() == "y":
-    for downscaled_image in downscaled_images:
-        horizontal_flip_image(downscaled_image, f"{downscaled_image}_flipped")
-        for i in range(1, 4):
-            rotate_image(downscaled_image, f"{downscaled_image}_rotated_{90*i}")
-            rotate_image(f"{downscaled_image}_flipped", f"{downscaled_image}_flipped_rotated_{90*i}")
+# f str(input("Augment images? Y/N: ")).lower() == "y":
+#     for downscaled_image in downscaled_images:
+#         horizontal_flip_image(downscaled_image, f"{downscaled_image}_flipped")
+#         for i in range(1, 4):
+#             rotate_image(downscaled_image, f"{downscaled_image}_rotated_{90*i}")
+#             rotate_image(f"{downscaled_image}_flipped", f"{downscaled_image}_flipped_rotated_{90*i}")

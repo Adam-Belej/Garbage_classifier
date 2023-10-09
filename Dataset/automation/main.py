c8f61ee15da7156d9fb5c115c74831b8e97528ca
Adam Belej <adam.belej@seznam.cz>
2023-10-09 17:24:31 +0200

    added code to main.py

--- Dataset/automation/main.py
+++ Dataset/automation/main.py
@@ -1,9 +1,28 @@
 #!/bin/python
 from ffmpeg_utils import *
 from katna_utils import *
+import os
 
 
 def main():
+
+    path_to_images = str(input("Folder with images: "))
+    original_images = get_all_images_from_dir(path_to_images)
+
+    cropped_path = f"{path_to_images}_cropped"
+
+    for image in original_images:
+        image = os.path.join(path_to_images, image)
+        crop_image_with_aspect_ratio(image, f"{path_to_images}_cropped")
+
+    downscaled_path = f"{cropped_path}_downscaled"
+    cropped_images = get_all_images_from_dir(cropped_path)
+
+    for cropped_image in cropped_images:
+        cropped_image = os.path.join(cropped_path, cropped_image)
+        downscaled_image = os.path.join(downscaled_path, cropped_image)
+        downscale_image(cropped_image, downscaled_image)
+
     pass
 
 

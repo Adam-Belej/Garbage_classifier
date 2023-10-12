58275b4271075bb94074c3a4e16c7e29b837100d
Adam Belej <adam.belej@seznam.cz>
2023-10-12 17:44:01 +0200

    changed from ffmpeg to pillow, added augmentation functions

--- Dataset/automation/pillow_utils.py
+++ Dataset/automation/pillow_utils.py
@@ -32,18 +32,19 @@ def horizontal_flip_image(input_file: str):
 
 def rotate_image(input_file: str,
                  angle: int = 90):
-    new_file = input_file.split(".")
 
     for i in range(1, 4):
 
+
+        new_file = input_file.split(".")
         new_file[0] = f"{new_file[0]}_rotated_{i}"
         output_file = ".".join(new_file)
 
-        angle *= i
+        new_angle = angle*i
 
         try:
             image = Image.open(input_file)
-            image = image.rotate(angle)
+            image = image.rotate(new_angle)
             image.save(output_file)
 
         except PIL.ImageFile.ERRORS as e:

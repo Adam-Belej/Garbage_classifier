66f41660a2d4ac068536fab771bc06600afd55bb
Adam Belej <adam.belej@seznam.cz>
2023-10-23 18:08:17 +0200

    added a func for creating datasts

---
+++ src/utlis/dataset_creation_utils.py
@@ -0,0 +1,21 @@
+import tensorflow as tf
+
+
+def create_dataset(data_dir: str,
+                   subset: str,
+                   validation_split: int = 0.2,
+                   img_width: int = 512,
+                   img_height: int = 512,
+                   batch_size: int = 32,
+                   seed: int = 123):
+    image_size = (img_height, img_width)
+    ds = tf.keras.utils.image_dataset_from_directory(
+        directory=data_dir,
+        subset=subset,
+        validation_split=validation_split,
+        image_size=image_size,
+        batch_size=batch_size,
+        seed=seed
+    )
+
+    return ds

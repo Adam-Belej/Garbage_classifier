3a1477902675535947916d257407c59d21ad8879
Jan Černý <jc@ucw.cz>
2023-10-24 17:21:38 +0200

    add classifier class

---
+++ src/models/Clasifier.py
@@ -0,0 +1,25 @@
+import typing
+
+class Classifier:
+
+
+    def __init__(self, model_path: str = None):
+        pass
+
+
+    def load_pretrained_model(self, path: str):
+        pass
+
+
+    def export_model(self, path: str):
+        pass
+
+
+    def train(self, data):
+        pass
+
+
+    def classify(self, data):
+        pass
+
+

9b69215079e09bc228fcf6119e1f6147b3933b0a
Adam Belej <adam.belej@seznam.cz>
2023-10-27 11:57:45 +0200

    added export and pretrained options to classifier

--- src/classifier.py
+++ src/classifier.py
@@ -4,13 +4,17 @@ import os
 
 
 def main(input_dir: str,
+         export_dir: str,
+         pretrained_dir: str,
          num_of_epochs: int = 10,
          num_of_classes: int = 3,
          img_width: int = 512,
          img_height: int = 512,
          batch_size: int = 3,
          validation_split: int = 0.2,
-         learning_rate=0.01):
+         learning_rate: int = 0.01,
+         pretrained: bool = False,
+         export: bool = False):
     training_set = nu.load_dataset(data_dir=input_dir,
                                    validation_split=validation_split,
                                    batch_size=batch_size,
@@ -24,16 +28,20 @@ def main(input_dir: str,
                                      img_height=img_height,
                                      subset="validation")
 
-    model = tf.keras.models.Sequential(
-        [
-            tf.keras.layers.Rescaling(1. / 255),
-            tf.keras.layers.Flatten(),
-            tf.keras.layers.Dense(units=128, activation='relu'),
-            tf.keras.layers.Dense(units=64, activation='relu'),
-            tf.keras.layers.Dense(units=32, activation='relu'),
-            tf.keras.layers.Dense(units=num_of_classes)
+    if pretrained:
+        model = tf.saved_model.load(pretrained_dir)
 
-        ])
+    else:
+        model = tf.keras.models.Sequential(
+            [
+                tf.keras.layers.Rescaling(1. / 255),
+                tf.keras.layers.Flatten(),
+                tf.keras.layers.Dense(units=128, activation='relu'),
+                tf.keras.layers.Dense(units=64, activation='relu'),
+                tf.keras.layers.Dense(units=32, activation='relu'),
+                tf.keras.layers.Dense(units=num_of_classes)
+
+            ])
 
     model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate),
                   loss=tf.keras.losses.SparseCategoricalCrossentropy())
@@ -41,6 +49,9 @@ def main(input_dir: str,
     model.fit(training_set,
               validation_data=validation_set,
               epochs=num_of_epochs)
+
+    if export:
+        tf.saved_model.save(model, export_dir=export_dir)
 
 
 if __name__ == "__main__":

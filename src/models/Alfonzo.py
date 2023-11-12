b5c2191cbd2dddc30a444ab74f7550885bd9c202
Adam Belej <adam.belej@seznam.cz>
2023-11-12 17:14:50 +0100

    update alfonzo

--- src/models/Alfonzo.py
+++ src/models/Alfonzo.py
@@ -4,17 +4,16 @@ from models.Clasifier import Classifier
 
 class Alfonzo(Classifier):
 
-    def __init__(self, num_of_classes):
+    def __init__(self,
+                 num_of_classes: int):
         super().__init__(num_of_classes)
         self.model = tf.keras.models.Sequential(
             [
                 tf.keras.layers.Rescaling(1. / 255),
                 tf.keras.layers.Flatten(),
-                tf.keras.layers.Dense(units=256, activation="relu"),
-                tf.keras.layers.Dense(units=128, activation="relu"),
-                tf.keras.layers.Dense(units=64, activation="tanh"),
-                tf.keras.layers.Dense(units=32, activation="tanh"),
-                tf.keras.layers.Dense(units=10, activation="relu"),
+                tf.keras.layers.Dense(units=128, activation='relu'),
+                tf.keras.layers.Dense(units=64, activation='relu'),
+                tf.keras.layers.Dense(units=32, activation='relu'),
                 tf.keras.layers.Dense(units=num_of_classes)
             ])
         self.model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.01),

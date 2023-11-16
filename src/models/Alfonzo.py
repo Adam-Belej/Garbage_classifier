4041aa01d03a15244dd3d69e49eba3f815f5ba03
Adam Belej <adam.belej@seznam.cz>
2023-11-16 19:06:05 +0100

    update alfonzo

--- src/models/Alfonzo.py
+++ src/models/Alfonzo.py
@@ -7,15 +7,18 @@ class Alfonzo(Classifier):
     def __init__(self,
                  num_of_classes: int):
         super().__init__(num_of_classes)
-        self.model = tf.keras.models.Sequential(
-            [
-                tf.keras.layers.Rescaling(1. / 255),
-                tf.keras.layers.Flatten(),
-                tf.keras.layers.Dense(units=128, activation='relu'),
-                tf.keras.layers.Dense(units=64, activation='relu'),
-                tf.keras.layers.Dense(units=32, activation='relu'),
-                tf.keras.layers.Dense(units=num_of_classes)
-            ])
+        self.model = tf.keras.Sequential([
+                        tf.keras.layers.Rescaling(1./255),
+                        tf.keras.layers.Conv2D(32, 3, activation='relu'),
+                        tf.keras.layers.MaxPooling2D(),
+                        tf.keras.layers.Conv2D(32, 3, activation='relu'),
+                        tf.keras.layers.MaxPooling2D(),
+                        tf.keras.layers.Conv2D(32, 3, activation='relu'),
+                        tf.keras.layers.MaxPooling2D(),
+                        tf.keras.layers.Flatten(),
+                        tf.keras.layers.Dense(128, activation='relu'),
+                        tf.keras.layers.Dense(num_of_classes)
+                        ])
         self.model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.01),
                            loss=tf.keras.losses.SparseCategoricalCrossentropy(),
                            metrics="accuracy")

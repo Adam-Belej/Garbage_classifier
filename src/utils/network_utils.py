28c632134ebd0324471945f12df9a5124807df3f
Adam Belej <adam.belej@seznam.cz>
2023-11-20 13:42:00 +0100

    add option to create dataset without split for testing

--- src/utils/network_utils.py
+++ src/utils/network_utils.py
@@ -6,23 +6,27 @@ import tensorflow as tf
 
 def load_dataset(data_dir: str,
                  subset: str,
-                 validation_split: int = 0.2,
+                 validation_split: int = None,
                  img_width: int = 512,
                  img_height: int = 512,
                  batch_size: int = 32,
                  seed: int = 123):
     image_size = (img_height, img_width)
-    ds = tf.keras.utils.image_dataset_from_directory(
-        directory=data_dir,
-        subset=subset,
-        validation_split=validation_split,
-        image_size=image_size,
-        batch_size=batch_size,
-        seed=seed
-    )
-
-    autotune = tf.data.AUTOTUNE
-    ds = ds.cache().prefetch(buffer_size=autotune)
+    if validation_split is not None:
+        ds = tf.keras.utils.image_dataset_from_directory(
+            directory=data_dir,
+            subset=subset,
+            validation_split=validation_split,
+            image_size=image_size,
+            batch_size=batch_size,
+            seed=seed
+        )
+    else:
+        ds = tf.keras.utils.image_dataset_from_directory(
+            directory=data_dir,
+            image_size=image_size,
+            batch_size=batch_size,
+        )
 
     return ds
 

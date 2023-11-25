4cdcff21aa0b4f772039a78e0cab24974637d545
Adam Belej <adam.belej@seznam.cz>
2023-11-25 11:32:00 +0100

    update oai

--- src/olomouc_ai_industries.py
+++ src/olomouc_ai_industries.py
@@ -10,39 +10,42 @@ def main(input_dir: str,
          img_width: int = 512,
          img_height: int = 512):
 
-    alfonz = Alfonzo(num_of_classes)
+    classifier = Alfonzo(num_of_classes)
 
     training_set = nu.load_dataset(data_dir=input_dir,
                                    img_width=img_width,
                                    img_height=img_height,
                                    subset="training",
-                                   validation_split=alfonz.valsplit,
+                                   validation_split=classifier.valsplit,
                                    seed=123,
-                                   batch_size=alfonz.batchsize)
+                                   batch_size=classifier.batchsize)
     validation_set = nu.load_dataset(data_dir=input_dir,
                                      img_width=img_width,
                                      img_height=img_height,
                                      subset="validation",
-                                     validation_split=alfonz.valsplit,
+                                     validation_split=classifier.valsplit,
                                      seed=123,
-                                     batch_size=alfonz.batchsize)
+                                     batch_size=classifier.batchsize)
 
-    alfonz.load_pretrained_model("/home/adam/Projects/Garbage_Classifier_dataset/test/model.keras")
-    alfonz.model.summary()
-    alfonz.train(training_data=training_set,
-                 validation_data=validation_set)
+    classifier.load_pretrained_model("/home/adam/Projects/Garbage_Classifier_dataset/test/model.keras")
+    classifier.model.summary()
+    classifier.train(training_data=training_set,
+                     validation_data=validation_set)
 
-    print("History is ", alfonz.history)
+    print("History is ", classifier.history)
 
-    alfonz.make_graph_from_history()
+    classifier.make_graph_from_history()
 
-    alfonz.test_accuracy(test_data_dir=test_dir,
-                         img_width=img_width,
-                         img_height=img_height)
-    print(alfonz.evaluation)
+    classifier.test_accuracy(test_data_dir=test_dir,
+                             img_width=img_width,
+                             img_height=img_height)
+    print(classifier.evaluation)
+
 
 if __name__ == "__main__":
     input_folder = input("Input directory: ")
     num_classes = int(input("Number of classes: "))
+    test_data = str(input("Folder containing test data: "))
     main(input_dir=input_folder,
-         num_of_classes=num_classes)
+         num_of_classes=num_classes,
+         test_dir=test_data)
